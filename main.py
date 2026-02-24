2a43b161fdeec74e032a6a5b31e0b8b09c1239bf
seanleeee13 <seanleeee13@gmail.com>
2026-02-24 22:32:35 +0900

    on working 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response, Depends, Cookie
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from pwdlib import PasswordHash
 from pwdlib.hashers.argon2 import Argon2Hasher
+from contextlib import asynccontextmanager
 import aiosqlite
 import datetime
 import dotenv
@@ -9,25 +11,40 @@ import jwt
 import re
 import os
 
-app = FastAPI()
+dotenv.load_dotenv()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    async with aiosqlite.connect("data.db") as con:
+        await con.execute(cols)
+        await con.commit()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("CLIENT_ADDRESS")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 pwd = PasswordHash(hashers=[Argon2Hasher])
 cols = """
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY UNIQUE KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
+    score INTEGER DEFAULT 0
 )
 """
-dotenv.load_dotenv()
 
-def create_token(data: dict, time: datetime.timedelta | None=datetime.timedelta(hours=1)):
-    if time:
-        expire = datetime.datetime.now(datetime.timezone.utc)
-        expire += time
-    else:
-        expire = None
-    return jwt.encode(data.copy().update({"exp": expire}), os.getenv("SECRET_KEY"))
+def create_token(data: dict, time: datetime.timedelta=datetime.timedelta(hours=24)):
+    expire = now = datetime.datetime.now(datetime.timezone.utc)
+    expire += time
+    payload = data.copy()
+    payload.update({"exp": expire, "iat": now})
+    return jwt.encode(payload, os.getenv("SECRET_KEY"))
 
 def decode_token(token: str):
     try:
@@ -44,12 +61,12 @@ async def root():
 
 @app.post("/user", status_code=status.HTTP_201_CREATED)
 async def signup(form_data: OAuth2PasswordRequestForm):
-    if not re.fullmatch("[A-Za-z_0-9]{6, 20}", form_data.username):
+    if not re.fullmatch("[A-Za-z_0-9]{6,20}", form_data.username):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bad username"
         )
-    if not re.fullmatch("[A-Za-z0-9!@#$%^&*-_+=\|/.,~]{8, 20}", form_data.password):
+    if not re.fullmatch("[A-Za-z0-9!@#$%^&*-_+=\|/.,~]{8,20}", form_data.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bad password"
@@ -70,13 +87,13 @@ async def signup(form_data: OAuth2PasswordRequestForm):
     return {"detail": "Created", }
 
 @app.post("/user/login")
-async def login(form_data: OAuth2PasswordRequestForm):
-    if not re.fullmatch("[A-Za-z_0-9]{6, 20}", form_data.username):
+async def login(response: Response, form_data: OAuth2PasswordRequestForm):
+    if not re.fullmatch("[A-Za-z_0-9]{6,20}", form_data.username):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bad username"
         )
-    if not re.fullmatch("[A-Za-z0-9!@#$%^&*-_+=\|/.,~]{8, 20}", form_data.password):
+    if not re.fullmatch("[A-Za-z0-9!@#$%^&*-_+=\|/.,~]{8,20}", form_data.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bad password"
@@ -89,9 +106,14 @@ async def login(form_data: OAuth2PasswordRequestForm):
             "SELECT * FROM users WHERE username = ?",
             (form_data.username, )
         )
-        user = user.fetchone()
+        user = await user.fetchone()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Wrong username or password"
+            )
         ver = pwd.verify_and_update(form_data.password, user["password"])
-        if user is None or not ver[0]:
+        if not ver[0]:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Wrong username or password"
@@ -101,5 +123,41 @@ async def login(form_data: OAuth2PasswordRequestForm):
                 "UPDATE OR IGNORE users SET password = ? WHERE username = ?",
                 (ver[1], form_data.username)
             )
-    create_token() # TODO: 여기부터 작성!
+            await con.commit()
+    token = create_token({"sub": form_data.username})
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,
+        max_age=604800,
+        expires=604800,
+        samesite="none",
+        secure=os.getenv("IS_PRODUCTION") == "true"
+    )
     return {"detail": "Success"}
+
+async def get_current_user(access_token: str=Cookie(None)):
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="" # not loged in? TODO 여기
+        )
+
+@app.post("/user/logout")
+async def logout(response: Response, score: int, current_user: str=Depends(get_current_user)): # TODO: 여기!
+    # 1. 점수 업데이트 (aiosqlite)
+    async with aiosqlite.connect("data.db") as con:
+        await con.execute(
+            "UPDATE users SET score = ? WHERE username = ?",
+            (score, current_user)
+        )
+        await con.commit()
+
+    # 2. 쿠키 삭제 (로그인 때와 옵션이 똑같아야 확실히 삭제됨)
+    response.delete_cookie(
+        key="access_token",
+        httponly=True,
+        secure=True,     # 배포 환경 기준
+        samesite="none"  # 배포 환경 기준
+    )
+    return {"detail": "Logged out and score saved"}
